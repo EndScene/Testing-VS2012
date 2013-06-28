cc0191c858339987ea379104f66ec9b0e6bbfd7d
EndScene <unrealprogrammer@live.de>
2013-06-28 21:53:34 +0200

    Test

---
+++ Test/Test.cpp
@@ -0,0 +1,14 @@
+// Test.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//test
+	std::cout << "test";
+	return 0;
+}
+
